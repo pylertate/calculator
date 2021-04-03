3424845752e3b4d59aa3c9cf83c68a9ca6050140
Tyler Pate <tylerpate96@gmail.com>
2021-04-03 18:09:27 -0400

    added functionality to the . still need to do %btn and figure out how to either round or ad del btn

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ const numBtn = document.querySelectorAll(".numBtn");
 const screen = document.querySelector("#display");
 const operator = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector("#clear");
+let equalBtn = document.querySelector("#equals");
+let periodBtn = document.querySelector("#period");
 let display = "";
 let a;
 let b;
 let o;
 let result;
 let isOperator = false;
+let isPeriod = false;
 
 numBtn.forEach((button) =>
   button.addEventListener("click", () => {
     if (display.length >= 9) return;
     else display += button.value;
-    screen.innerHTML = `${display}`;
+    screen.innerHTML = display;
   })
 );
 
@@ -23,29 +26,58 @@ operator.forEach((button) =>
       case (isOperator = false):
         a = display;
         o = button.value;
+        console.log(o);
         display = "";
         screen.innerHTML = display;
         isOperator = true;
+        isPeriod = false;
         break;
       case (isOperator = true):
         b = display;
         operate(a, b, o);
         screen.innerHTML = result;
-        a=result
-        display = ""
-        o=button.value
+        a = result;
+        b = "";
+        display = "";
+        o = button.value;
+        isPeriod = false;
         break;
     }
   })
 );
+
 clearBtn.addEventListener("click", () => {
   clear();
-  a =undefined
-  b=undefined
-  display=""
-  o=undefined
-  isOperator=false
+  a = undefined;
+  b = undefined;
+  o = undefined;
+  isOperator = false;
+  screen.innerHTML = 0;
+  display = "";
+  isPeriod = false;
+});
 
+equalBtn.addEventListener("click", () => {
+  b = display;
+  operate(a, b, o);
+  screen.innerHTML = result;
+  a = result;
+  b = "";
+  display = "";
+  isPeriod = false;
+  return;
+});
+periodBtn.addEventListener("click", () => {
+  switch (isPeriod) {
+    case (isPeriod = false):
+      display = display + ".";
+      isPeriod = true;
+      screen.innerHTML = display;
+      console.log("it works");
+      break;
+    case (isPeriod = true):
+      break;
+  }
 });
 add = (a, b) => {
   return Number(a) + Number(b);
@@ -57,7 +89,8 @@ multiply = (a, b) => {
   return a * b;
 };
 divide = (a, b) => {
-  return a / b;
+  if (b == 0) return "pain";
+  else return a / b;
 };
 
 operate = (a, b, o) => {
@@ -71,15 +104,17 @@ operate = (a, b, o) => {
     case (o = "*"):
       result = multiply(a, b);
       break;
-    case (o = "/"):
+    case (o = "÷"):
       result = divide(a, b);
       break;
-    case (o="="):
-        o=""
-
-    break;
   }
-  result = result
+  if (result> 999999999) result = "error";
+  if (result.toString().length>9){
+      decimal(result)
+  }
+  
+  else return result;
+
   return;
 };
 
